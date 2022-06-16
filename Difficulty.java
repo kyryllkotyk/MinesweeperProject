cf29383284908ed8cf554ddf01a239bc5c1b33d2
kyryllkotyk <105020476+kyryllkotyk@users.noreply.github.com>
2022-06-16 14:59:55 -0700

    Update Difficulty.java

--- Difficulty.java
+++ Difficulty.java
@@ -1,34 +1,38 @@
+//Students Alisa Bajenova, Kyryll Kotyk, Eric Lindquist
+// 6.16.2022
+// CSE 142 AP COMP SCI
+//This class will return the information about
+//rows, columns, bombs, and tile size
 public class Difficulty{
-   public String diff;
    /**
     *Constructs the class
     */
    public Difficulty(){
-      diff = "Normal";
+     
    }
    /**
     *@return the row count
     */
    public int rowCount(){
-         return 14;
+      return 14;
    }
    /**
     *@return the column count
     */
    public int colCount(){
-         return 18;
+      return 18;
    }
    /**
     *@return the size of the tiles
     */
    public int tileSize(){
-         return 40;
+      return 40;
    }
    /**
     *@return the number of mines
     */
    public int getNumberOfMines(){
-         return 40;
+      return 40;
    }
   
 }

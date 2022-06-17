1f18ec61494bebbe75cd0557eab5a59ccbaf4c1c
kyryllkotyk <105020476+kyryllkotyk@users.noreply.github.com>
2022-06-16 21:31:55 -0700

    Updated Class comment

--- PlayAgainListener.java
+++ PlayAgainListener.java
@@ -1,14 +1,15 @@
 // Students Alisa Bajenova, Kyryll Kotyk, Eric Lindquist
 // 6.16.2022
 // CSE 142 AP COMP SCI
-// play again button Listener
-// will detect if the play again button is pressed.
-// If so it will start a new game
 
 import java.awt.event.*;
 import javax.swing.*;
 import java.awt.*;
-
+/**
+ *Play again button listener
+ *Will detect if the play again buttton is pressed
+ *If so it will start a new game
+ */
 public class PlayAgainListener implements ActionListener{
    /**
    * if it detects a click it starts a new program
